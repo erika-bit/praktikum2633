200fc2637b561b6e68e6bb38ce72ea809956f94d
erika-bit <erikaputri@students.amikom.ac.id>
2020-03-11 18:36:23 +0700

    menambahkan perkalian pembagian

--- calculator2633.cs
+++ calculator2633.cs
@@ -4,16 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-class calculator
+namespace Calculator
 {
-  static void main (string[] args)
+  class calculator
   {
-static int Penambahan(int a, int b){
-  return a + b;
-}
-static int Pengurangan(int a, int b){
-
-}
+    static void main (string[] args)
+    {
+      int a =10;
+      int b =6;
+      Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}",a, b, Penambahan(a, b));
+      Console.WriteLine("Hasil Pengurangan: {0} + {1} = {2}",a, b, Pengurangan(a,b));
+      Console.WriteLine("Hasil Perkalian: {0} * {1} = {2}",a, b, Perkalian(a,b));
+      Console.WriteLine("Hasil Pembagian: {0} / {1} = {2}",a, b, Pembagian(a,b));
+      
+        static int Penambahan(int a, int b){
+            return a + b;
+        }
+        static int Pengurangan(int a, int b){
+          return a - b;
+        }
+    }
   }
 }
