d0c290d308853d7228b9e87418d0b0e45541b7f6
erika-bit <erikaputri@students.amikom.ac.id>
2020-03-11 18:11:46 +0700

    menambahkan fungsi penambhan dan pengurangan

--- calculator2633.cs
+++ calculator2633.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
 class calculator
 {
   static void main (string[] args)
   {
+static int Penambahan(int a, int b){
+  return a + b;
+}
+static int Pengurangan(int a, int b){
 
+}
   }
 }
